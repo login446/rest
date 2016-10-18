4b5d0ee4cc1584ce3b7acc63df5a09c5fd69c736
Lesha Filipovich <lesha_philip@mail.ru>
2016-10-19 00:06:59 +0300

    first commit

---
+++ src/main/java/com/alex/rest/Greeting.java
@@ -0,0 +1,22 @@
+package com.alex.rest;
+
+/**
+ * Created by alex on 18.10.2016.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
