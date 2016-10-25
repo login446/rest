5592d7304bf77e0ba531029fbda50ce2aac30357
Lesha Filipovich <lesha_philip@mail.ru>
2016-10-25 22:11:51 +0300

    new test commit

--- src/test/com/alex/rest/WebControllerTest.java
+++ src/test/com/alex/rest/WebControllerTest.java
@@ -1,19 +1,19 @@
 package com.alex.rest;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
-import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
-import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,25 +31,148 @@ public class WebControllerTest {
 
     private MockMvc mockMvc;
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Before
     public void setUp() throws Exception {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
+
+        String sql = "CREATE TABLE users " +
+                "(id INT NOT NULL AUTO_INCREMENT," +
+                "name VARCHAR(45) NOT NULL," +
+                "score INT NOT NULL," +
+                " PRIMARY KEY (id))";
+        jdbcTemplate.execute(sql);
+
+        String sqlInsert = "INSERT INTO users(name, score) VALUES(?,?)";
+        jdbcTemplate.update(sqlInsert, "ConflictName", 0);
+        jdbcTemplate.update(sqlInsert, "DeleteName", 0);
+        jdbcTemplate.update(sqlInsert, "Rename", 0);
+        jdbcTemplate.update(sqlInsert, "RenameOk", 0);
+        jdbcTemplate.update(sqlInsert, "ReScore", 0);
+    }
+
+    @After
+    public void drop() throws Exception {
+        String sql = "DROP TABLE users";
+        jdbcTemplate.execute(sql);
     }
 
     @Test
-    public void addNewUserBadRequest() throws Exception {
+    public void addNewUserIsEmptyName() throws Exception {
         mockMvc.perform(put("/user")
                 .param("name", ""))
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void addNewUserConflict() throws Exception {
+        mockMvc.perform(put("/user")
+                .param("name", "ConflictName"))
+                .andExpect(status().isConflict());
+    }
+
     @Test
     public void addNewUser() throws Exception {
         mockMvc.perform(put("/user")
                 .param("name", "Joni"))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.userId").value("1"))
-                .andExpect(jsonPath("$.firstName").value("Joni"))
-                .andExpect(jsonPath("$.lastName").value("44"));
+                .andExpect(jsonPath("$.id").value("6"))
+                .andExpect(jsonPath("$.name").value("Joni"))
+                .andExpect(jsonPath("$.score").value("0"));
     }
 
+    @Test
+    public void delUserIsEmptyName() throws Exception {
+        mockMvc.perform(delete("/user")
+                .param("name", ""))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void delUserNotFound() throws Exception {
+        mockMvc.perform(delete("/user")
+                .param("name", "DeleteNameNotFound"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void delUser() throws Exception {
+        mockMvc.perform(delete("/user")
+                .param("name", "DeleteName"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void renameIsEmptyName() throws Exception {
+        mockMvc.perform(patch("/user/name")
+                .param("name", "")
+                .param("newName", "freeName"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void renameIsEmptyNewName() throws Exception {
+        mockMvc.perform(patch("/user/name")
+                .param("name", "Rename")
+                .param("newName", ""))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void renameNotFound() throws Exception {
+        mockMvc.perform(patch("/user/name")
+                .param("name", "renameNotFound")
+                .param("newName", "freeName"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void renameConflict() throws Exception {
+        mockMvc.perform(patch("/user/name")
+                .param("name", "Rename")
+                .param("newName", "ConflictName"))
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void rename() throws Exception {
+        mockMvc.perform(patch("/user/name")
+                .param("name", "RenameOk")
+                .param("newName", "freeNameOk"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void reScoreIsEmpty() throws Exception {
+        mockMvc.perform(patch("/user/score")
+                .param("name", "")
+                .param("score", "55"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void reScoreNumberFormat() throws Exception {
+        mockMvc.perform(patch("/user/score")
+                .param("name", "reScore")
+                .param("score", "55t"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void reScoreNotFound() throws Exception {
+        mockMvc.perform(patch("/user/score")
+                .param("name", "reScoreNotFound")
+                .param("score", "55"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void reScore() throws Exception {
+        mockMvc.perform(patch("/user/score")
+                .param("name", "reScore")
+                .param("score", "55"))
+                .andExpect(status().isOk());
+    }
 }
